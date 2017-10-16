77cfe5725cf6bb8ea191070bb0c5733dbdfee03a
LiangYi <liangyi@mallcoo.cn>
2017-10-16 19:19:05 +0800

    up

---
+++ src/Raven/Raven/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raven.Attributes
+{
+    /// <summary>指定属性或事件的描述。</summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class DescriptionAttribute : Attribute
+    {
+        /// <summary>
+        ///   指定的默认值为 <see cref="T:System.ComponentModel.DescriptionAttribute" />, ，这是一个空字符串 ("")。
+        ///    这 <see langword="static" /> 字段是只读的。
+        /// </summary>
+        public static readonly DescriptionAttribute Default = new DescriptionAttribute();
+        private string description;
+
+        /// <summary>
+        ///   新实例初始化 <see cref="T:System.ComponentModel.DescriptionAttribute" /> 不带参数的类。
+        /// </summary>
+        public DescriptionAttribute()
+          : this(string.Empty)
+        {
+        }
+
+        /// <summary>
+        ///   新实例初始化 <see cref="T:System.ComponentModel.DescriptionAttribute" /> 类的说明。
+        /// </summary>
+        /// <param name="description">说明文本中。</param>
+        public DescriptionAttribute(string description)
+        {
+            this.description = description;
+        }
+
+        /// <summary>获取存储在此属性的说明。</summary>
+        /// <returns>此属性中存储的说明。</returns>
+        public virtual string Description
+        {
+            get
+            {
+                return this.DescriptionValue;
+            }
+        }
+
+        /// <summary>获取或设置作为说明存储的字符串。</summary>
+        /// <returns>
+        ///   描述作为存储的字符串。
+        ///    默认值为空字符串 ("")。
+        /// </returns>
+        protected string DescriptionValue
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        /// <summary>
+        ///   返回给定对象的值是否等于当前 <see cref="T:System.ComponentModel.DescriptionAttribute" />。
+        /// </summary>
+        /// <param name="obj">要测试值的相等性的对象。</param>
+        /// <returns>
+        ///   <see langword="true" /> 如果给定对象的值是否等于当前;否则为 <see langword="false" />。
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
+            DescriptionAttribute descriptionAttribute = obj as DescriptionAttribute;
+            if (descriptionAttribute != null)
+                return descriptionAttribute.Description == this.Description;
+            return false;
+        }
+
+        /// <summary>返回此实例的哈希代码。</summary>
+        /// <returns>32 位有符号整数哈希代码。</returns>
+        public override int GetHashCode()
+        {
+            return this.Description.GetHashCode();
+        }
+        
+    }
+
+}
---
+++ src/Raven/Raven/Extensions/EnumExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Raven.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Raven.Extensions
+{
+    public static class EnumExtensions
+    {
+        private static Dictionary<Type, Dictionary<int, string>> enumtDescriptionDict = new Dictionary<Type, Dictionary<int, string>>();
+        private static object objLock = new object();
+        private static Type enumType = typeof(Enum);
+
+        /// <summary>
+        /// 扩展方法，获得枚举的Description
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <returns>枚举的Description</returns>
+        public static string GetDesc(this Enum value)
+        {
+            string name = value.ToString();
+            if (name == null)
+            {
+                return null;
+            }
+            Type type = value.GetType();
+            if (!enumtDescriptionDict.ContainsKey(type))
+            {
+                lock (objLock)
+                {
+                    if (!enumtDescriptionDict.ContainsKey(type))
+                    {
+                        enumtDescriptionDict[type] = EnumToDictionary(type);
+                    }
+                }
+            }
+
+            int key = Convert.ToInt32(value);
+            return enumtDescriptionDict[type][key];
+        }
+
+        /// <summary>
+        /// 把枚举转换为键值对集合
+        /// </summary>
+        /// <param name="enumType">枚举类型</param>
+        /// <returns>以枚举值为key，枚举文本为value的键值对集合</returns>
+        private static Dictionary<int, string> EnumToDictionary(Type enumType)
+        {
+            if (!EnumExtensions.enumType.Equals(enumType))
+            {
+                throw new ArgumentException("传入的参数必须是枚举类型！", "enumType");
+            }
+            Dictionary<int, string> enumDic = new Dictionary<int, string>();
+            Array enumValues = Enum.GetValues(enumType);
+            foreach (Enum enumValue in enumValues)
+            {
+                int key = Convert.ToInt32(enumValue);
+                string enumName = enumValue.ToString();
+                FieldInfo field = enumType.GetField(enumName);
+
+                DescriptionAttribute attribute = CustomAttributeExtensions.GetCustomAttribute<DescriptionAttribute>(field);
+                //DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    enumName = attribute.Description;
+                }
+                enumDic.Add(key, enumName);
+            }
+            return enumDic;
+        }
+
+    }
+}
